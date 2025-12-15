064faebb7c5f24bb4f2d5e3fb0ef6024ac147bec
Kerosene <yoursecretnooby@gmail.com>
2025-12-15 22:47:44 +0800

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,42 @@
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://jitpack.io")
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenLocal()
+        mavenCentral()
+        maven("https://repo.opencollab.dev/maven-snapshots")
+        maven("https://repo.opencollab.dev/maven-releases")
+
+    }
+}
+
+rootProject.name = "TrinityClient"
+include(":app")
+include(":relay")
+include(":desktop")
+include(
+    ":relay:adventure",
+    ":relay:Protocol:bedrock-codec",
+    ":relay:Protocol:bedrock-connection",
+    ":relay:Protocol:common",
+    ":relay:Network:codec-query",
+    ":relay:Network:codec-rcon",
+    ":relay:Network:transport-raknet",
+)
